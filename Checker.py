c7831ccd14b3fab52100cdabc5efd51472fcdab0
AsuleJonathan <asulejona@gmail.com>
2023-10-19 11:18:32 +0300

    added python file to check for specific files

---
+++ Checker.py
@@ -0,0 +1,18 @@
+import os 
+
+# Specify path 
+path = '/usr/local/bin/'
+
+# Check whether the specified 
+# path exists or not 
+isExist = os.path.exists(path) 
+print(isExist) 
+
+
+# Specify path 
+path = '/home/User/Desktop/file.txt'
+
+# Check whether the specified 
+# path exists or not 
+isExist = os.path.exists(path) 
+print(isExist)
